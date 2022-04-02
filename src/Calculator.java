df582557abc0150357fc8403bb10707ad9bf4f79
Aleksei Grinko <als.grinko@yandex.ru>
2022-04-02 20:38:37 +0400

    feat: реализовал калькулятор

---
+++ src/Calculator.java
@@ -0,0 +1,90 @@
+import java.util.Stack;
+
+public class Calculator {
+    public String converterForNotation(String string) {
+        String notation = "";
+        Stack<Character> stack = new Stack<>();
+        int grade;
+        for (int i = 0; i < string.length(); i++) {
+            grade = rotation(string.charAt(i));
+            if (grade == 0) {
+                notation += string.charAt(i);
+            } else if (grade == 1) {
+                stack.push(string.charAt(i));
+            } else if (grade > 1) {
+                notation += ' ';
+                while (!stack.empty()) {
+                    if (grade <= rotation(stack.peek())) {
+                        notation += stack.pop();
+                    } else {
+                        break;
+                    }
+                }
+                stack.push(string.charAt(i));
+            } else if (grade == -1) {
+                notation += ' ';
+                while (rotation(stack.peek()) != 1) {
+                    notation += stack.pop();
+                }
+                stack.pop();
+            }
+        }
+        while (!stack.empty()) {
+            notation += stack.pop();
+        }
+        return notation;
+    }
+
+    public Double calc(String string){
+        String notation = converterForNotation(string);
+        String str = new String();
+        Stack<Double> stack = new Stack<>();
+        for (int i = 0; i < notation.length(); i++) {
+            if (notation.charAt(i) == ' ') {
+                continue;
+            }
+            if (rotation(notation.charAt(i)) == 0) {
+                while (rotation(notation.charAt(i)) == 0 && notation.charAt(i) != ' ') {
+                    str += notation.charAt(i);
+                    i++;
+                    if (i == notation.length()) {
+                        break;
+                    }
+                }
+                stack.push(Double.parseDouble(str));
+                str = new String();
+            }
+            if (rotation(notation.charAt(i)) > 1) {
+                Double a = stack.pop();
+                Double b = stack.pop();
+                if (notation.charAt(i) == '+') {
+                    stack.push(b + a);
+                }
+                if (notation.charAt(i) == '-') {
+                    stack.push(b - a);
+                }
+                if (notation.charAt(i) == '*') {
+                    stack.push(b * a);
+                }
+                if (notation.charAt(i) == '/') {
+                    stack.push(b / a);
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public int rotation(char example) {
+        if (example == '*' || example == '/') {
+            return 3;
+        } else if (example == '+' || example == '-') {
+            return 2;
+        } else if (example == '(') {
+            return 1;
+        } else if (example == ')') {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        String example1 = "1 + 2";
+        System.out.println("1-й пример: " + example1);
+        System.out.println("Ответ: " + calculator.calc(example1));
+        String example2 = "5 - 4 * 2";
+        System.out.println("2-й пример: " + example2);
+        System.out.println("Ответ: " + calculator.calc(example2));
+        String example3 = "4 + 6 / (12 - 3 * 2)";
+        System.out.println("3-й пример: " + example3);
+        System.out.println("Ответ: " + calculator.calc(example3));
+
+        System.out.println("Введите произвольный пример:");
+        String example = scanner.nextLine();
+        System.out.println(calculator.calc(example));
+    }
+}
